2ad02ba863c8e3703c306b47a20a27c6526905f9
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2024-09-28 23:30:06 -0400

    Revert rover-control.py

--- hardware/API/rover-control.py
+++ hardware/API/rover-control.py
@@ -13,11 +13,9 @@ load_dotenv()
 app = FastAPI()
 
 # CORS configuration
-ALLOWED_ORIGIN = os.environ.get("ALLOWED_ORIGIN")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGIN,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
